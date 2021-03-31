dfc00497b39cdd7efb7f40d35715e73e5651877b
Andrés Hadechine <andreshadechine@gmail.com>
2021-03-31 00:52:24 -0500

    Implementación de clase Part07Errors

--- src/main/java/co/com/sofka/Part07Errors.java
+++ src/main/java/co/com/sofka/Part07Errors.java
@@ -18,24 +18,41 @@ package co.com.sofka;
 
 
 import co.com.sofka.domain.User;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Part07Errors {
 
 
-	public Mono<User> betterCallSaulForBogusMono(Mono<Object> error) {
-		return null;
+	public Mono<User> betterCallSaulForBogusMono(Mono<User> error) {
+		return error.onErrorReturn(User.SAUL);
 	}
 
-	public Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<Object> error) {
-		return null;
+	public Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> error) {
+		return error.onErrorResume(e -> Flux.just(User.SAUL, User.JESSE));
 	}
 
 	public Flux<User> capitalizeMany(Flux<User> just) {
-		return null;
+		return just.map(
+				u -> {
+					try {
+						return capitalizeUser(u);
+					} catch (GetOutOfHereException e) {
+						throw Exceptions.propagate(e);
+					}
+				}
+		);
+	}
+
+	User capitalizeUser(User user) throws GetOutOfHereException {
+		if (user.equals(User.SAUL)) {
+			throw new GetOutOfHereException();
+		}
+		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
 	}
 
 	public class GetOutOfHereException extends Exception{
+		private static final long serialVersionUID = 0L;
 	}
 }
